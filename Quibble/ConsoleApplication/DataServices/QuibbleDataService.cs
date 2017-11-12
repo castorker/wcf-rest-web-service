f6dbc3ac60cf2b85eefc2adb6f55bdb9450e476e
Eduardo <eduardo.cxcorreio@gmail.com>
2017-11-12 17:21:01 +0100

    Added code to Client scaffolding console application.

---
+++ Quibble/ConsoleApplication/DataServices/QuibbleDataService.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleApplication.Transports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication.DataServices
+{
+    class QuibbleDataService
+    {
+        public QuibbleDataService()
+        {
+
+        }
+
+        public  Quibble[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Quibble GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Quibble quibble)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Quibble quibble)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
